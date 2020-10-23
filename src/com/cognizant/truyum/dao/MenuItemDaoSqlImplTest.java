198f9ec458584cc8ece992152678bd37a75dcb9c
Akhansha <akhanshaprashil@gmail.com>
2020-10-23 12:56:28 +0530

    changes made

--- src/com/cognizant/truyum/dao/MenuItemDaoSqlImplTest.java
+++ src/com/cognizant/truyum/dao/MenuItemDaoSqlImplTest.java
@@ -2,4 +2,19 @@ package com.cognizant.truyum.dao;
 
 public class MenuItemDaoSqlImplTest {
 
+	public static void main(String[] args) {
+		
+	}
+	public void testGetMenuItemListAdmin() {
+		
+	}
+	public void testGetMenuItemListCustomer() {
+		
+	}
+	public void testModifyMenuItem() {
+		
+	}
+	public void GetMenuItem() {
+		
+	}
 }
